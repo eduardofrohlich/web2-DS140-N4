35bb60a06109409223ab581d37eeb1e2088bb8f0
Eduardo Vinicius Frohlich <86862398+eduardofrohlich@users.noreply.github.com>
2023-11-10 14:34:16 -0300

    atualiza instancia Cliente

--- server/src/main/java/backend/lavanderia/config/TestConfig.java
+++ server/src/main/java/backend/lavanderia/config/TestConfig.java
@@ -33,10 +33,10 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Override
 	public void run(String... args) throws Exception {
-		Cliente c1 = new Cliente("Endereco XV", "1234", "joao@gmail.com", "João", "11111111111", "4136210493");
-		Cliente c2 = new Cliente("Endereco XX", "1235", "jose@gmail.com", "José", "11111111112", "4136210494");
-		Cliente c3 = new Cliente("Endereco XY", "1236", "joana@gmail.com", "Joana", "11111111113", "4136210495");
-		Cliente c4 = new Cliente("Endereco XZ", "1237", "joaquina@gmail.com", "Joaquina", "11111111114", "4136210496");
+		Cliente c1 = new Cliente(null, "Endereco XV", "1234", "joao@gmail.com", "João", "11111111111", "4136210493");
+		Cliente c2 = new Cliente(null, "Endereco XX", "1235", "jose@gmail.com", "José", "11111111112", "4136210494");
+		Cliente c3 = new Cliente(null, "Endereco XY", "1236", "joana@gmail.com", "Joana", "11111111113", "4136210495");
+		Cliente c4 = new Cliente(null, "Endereco XZ", "1237", "joaquina@gmail.com", "Joaquina", "11111111114", "4136210496");
 		clientRepository.saveAll(Arrays.asList(c1, c2, c3, c4));
 		
 		Funcionario f1 = new Funcionario(null, "Endereco XA", "1234", "maria@gmail.com", "Maria", "1111122222", "4136210497", "11/10/1990");
