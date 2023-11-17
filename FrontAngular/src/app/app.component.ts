905a5de6efcf33bdcadaeb9ee2eedd7fa36e2838
Gustavo Nielsen <gustavo.iassyl@gmail.com>
2023-11-17 00:05:24 -0300

    Carregar dados do banco tela inicial

--- FrontAngular/src/app/app.component.ts
+++ FrontAngular/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TelaInicialService } from './tela-inicial.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-tela-inicial',
+  templateUrl: './tela-inicial.component.html',
+  styleUrls: ['./tela-inicial.component.css']
 })
-export class AppComponent {
-  title = 'FrontAngular';
+export class TelaInicialComponent implements OnInit {
+  dadosDoBanco: any[] = [];
+
+  constructor(private telaInicialService: TelaInicialService) { }
+
+  ngOnInit(): void {
+    this.carregarDadosDoBanco();
+  }
+
+  carregarDadosDoBanco() {
+    this.telaInicialService.getDadosDoBanco().subscribe(
+      (dados) => {
+        this.dadosDoBanco = dados;
+      },
+      (error) => {
+        console.error('Erro ao carregar dados do banco', error);
+      }
+    );
+  }
 }
