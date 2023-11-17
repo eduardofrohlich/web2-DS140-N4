f7a109552ac2fee36e926500aae49bf5fcb41534
eduardofrohlich <eduardofrohlich121@gmail.com>
2023-11-17 02:27:43 -0300

    add tela login

--- FrontAngular/src/app/app-routing.module.ts
+++ FrontAngular/src/app/app-routing.module.ts
@@ -3,16 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { PagarPedidoComponent } from './cliente/pagar-pedido/pagar-pedido.component';
 import { PedidoOnlineComponent } from './cliente/pedido-online/pedido-online.component';
 import { TelaInicialComponent } from './cliente/tela-inicial/tela-inicial.component';
+import { LoginComponent } from './auth/login/login.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'cliente/pedidoonline', pathMatch: 'full'},
-  {path: 'cliente/pagarpedido/:id', component: PagarPedidoComponent},
-  {path: 'cliente/pedidoonline', component: PedidoOnlineComponent},
-  { path: 'cliente/inicial', component: TelaInicialComponent }
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: 'cliente/pedidoonline', pathMatch: 'full' },
+  { path: 'cliente/pagarpedido/:id', component: PagarPedidoComponent },
+  { path: 'cliente/pedidoonline', component: PedidoOnlineComponent },
+  { path: 'cliente/inicial', component: TelaInicialComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
