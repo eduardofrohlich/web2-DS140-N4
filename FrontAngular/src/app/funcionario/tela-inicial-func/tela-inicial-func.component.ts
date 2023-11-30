af53a3fc822aad95f8ec152cf8ca138963d30bf7
Gabriel F B Santos <gfbsant@gmail.com>
2023-11-30 02:27:17 -0300

    implementa tela meus pedidos

--- FrontAngular/src/app/funcionario/tela-inicial-func/tela-inicial-func.component.ts
+++ FrontAngular/src/app/funcionario/tela-inicial-func/tela-inicial-func.component.ts
@@ -1,14 +1,6 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
 import { Pedido } from 'src/app/shared';
 import { PedidosService } from '../services/pedidos.service';
-
-const httpHeader = {
-  headers: new HttpHeaders({
-    'Content-Type': 'application/json'
-  })
-};
 
 @Component({
   selector: 'app-tela-inicial-func',
@@ -21,14 +13,10 @@ export class TelaInicialFuncComponent implements OnInit {
   pedidos!: Pedido[];
 
   @ViewChild('content', { static: false }) el!: ElementRef;
-  constructor(private http: HttpClient, private router: Router, private route: ActivatedRoute, private pedidoService: PedidosService) { }
+  constructor(private pedidoService: PedidosService) { }
 
   ngOnInit(): void {
     this.carregarDados();
-    if (this.pedidos.length === 0) {
-      ///quero que seja exibida uma mensagem de não há pedidos caso a variavel pedidos[] esteja sem elementos
-     }
-
   }
 
   carregarDados() {
@@ -42,16 +30,10 @@ export class TelaInicialFuncComponent implements OnInit {
       if (pedido.idPedido !== undefined && pedido.pedidoStatus !== undefined) {
         const id: number = pedido.idPedido;
         const estado: string = pedido.pedidoStatus.toString();
-        const indicePedido = this.pedidos.findIndex(p => p.idPedido === id);
-        if (indicePedido !== -1) {
-          this.pedidos.splice(indicePedido, 1);
           this.pedidoService.atualizarEstado(id, estado);
-          this.carregarDados(); 
         } else {
           console.error('Pedido não encontrado na lista.');
         }
       }
-    }
-
   }
 }
