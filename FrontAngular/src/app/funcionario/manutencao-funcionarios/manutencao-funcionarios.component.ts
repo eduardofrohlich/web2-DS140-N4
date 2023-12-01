8bf470f5b66c5ad730ce5dd85288854293f89fec
Gabriel F B Santos <gfbsant@gmail.com>
2023-11-30 22:19:12 -0300

    adiciona componente manutencao de funcionarios

--- FrontAngular/src/app/funcionario/manutencao-funcionarios/manutencao-funcionarios.component.ts
+++ FrontAngular/src/app/funcionario/manutencao-funcionarios/manutencao-funcionarios.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Funcionario } from 'src/app/shared';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FuncionarioService } from '../services/funcionario.service';
+
 
 @Component({
   selector: 'app-manutencao-funcionarios',
   templateUrl: './manutencao-funcionarios.component.html',
   styleUrls: ['./manutencao-funcionarios.component.css']
 })
-export class ManutencaoFuncionariosComponent {
+export class ManutencaoFuncionariosComponent implements OnInit {
 
+  funcionarios!: Funcionario[];
+  @ViewChild('content', { static: false }) el!: ElementRef;
+  constructor(private http: HttpClient, private router: Router, private route: ActivatedRoute, private funcionarioService: FuncionarioService) { }
+
+  carregarDados() {
+    this.funcionarioService.getFuncionarios().subscribe((data: Funcionario[]) => {
+      this.funcionarios = data;
+    });
+  }
+
+  ngOnInit(): void {
+    this.carregarDados();
+  }
+
+  exibirDetalhes(func : Funcionario) {
+    this.router.navigate(['../funcionarios/detalhes-funcionario', func.idFuncionario], { relativeTo: this.route });
+  }
 }
--- FrontAngular/src/app/funcionario/services/pedidos.service.ts
+++ FrontAngular/src/app/funcionario/services/pedidos.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, catchError, map, throwError } from 'rxjs';
-import { Pedido } from 'src/app/shared';
 
 const httpHeader = {
   headers: new HttpHeaders({
